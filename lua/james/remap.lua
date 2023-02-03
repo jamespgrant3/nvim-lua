95b7670f99d3a52efa418e98bda3777d7a4432d4
James Grant <jamespgrant3@gmail.com>
2023-02-03 07:14:15 -0500

    minor tweaks to netrw, added some descriptions

--- lua/james/remap.lua
+++ lua/james/remap.lua
@@ -1,8 +1,11 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>nr", vim.cmd.Ex, { desc = "open [n]et[r]w" })
+-- view all keybindings with :map
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>nr", vim.cmd.Lexplore, { desc = "open [n]et[r]w vertical" })
+vim.keymap.set("n", "<leader>nrh", vim.cmd.Hex, { desc = "open [n]et[r]w [h]orizontal" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move line up" })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -14,25 +17,24 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "map to avoid losing what is in the paste register when you `vip`" })
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- vertical insert, have to press esc, map c-c to esc
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "q:", "<nop>")
 vim.keymap.set("n", "Q", "<nop>")
---vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "format the current buffer" })
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "string replace" })
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>q", "<cmd>silent :q<CR>")
@@ -53,4 +55,4 @@ vim.keymap.set("n", "=", "<cmd>silent :tabnext<CR>")
 vim.keymap.set("n", "<leader>nt", "<cmd>silent :tabnew<CR>")
 
 -- toggle line numbering
-vim.keymap.set("n", "<leader>rn", "<cmd>silentn:set rnu!<CR>")
+vim.keymap.set("n", "<leader>ln", "<cmd>silent :set rnu!<CR>", { desc = "toggle relative line numbering" })
