e17167e357a096fb7d70033b151913fc9cd802b9
James Grant <jamespgrant3@gmail.com>
2023-05-03 15:37:32 -0400

    updated update_on_change_command to be empty

--- after/plugin/git-worktree.lua
+++ after/plugin/git-worktree.lua
@@ -1,1 +1,9 @@
+require("git-worktree").setup({
+    change_directory_command = "cd",
+    update_on_change = true,
+    update_on_change_command = "",
+    clearjumps_on_change = true,
+    autopush = false,
+})
+
 require("telescope").load_extension("git_worktree")
