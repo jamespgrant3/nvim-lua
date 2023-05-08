6acc22105b414824afab8a4872704b5b0771988f
James Grant <jamespgrant3@gmail.com>
2023-05-08 13:34:10 -0400

    updated keystroke descriptions

--- after/plugin/vim-bujo.lua
+++ after/plugin/vim-bujo.lua
@@ -1,12 +1,11 @@
-vim.keymap.set("n", "<leader>tdl", "<cmd>silent :Todo<cr>", { desc = "open or close the tree" })
-vim.keymap.set("n", "<leader>tda", "<plug>silent :BujoAddnormal", { desc = "open or close the tree" })
+vim.keymap.set("n", "<leader>tdl", "<cmd>silent :Todo<cr>", { desc = "open or close the todo list" })
 
 -- add
-vim.keymap.set("n", "<C-a>", "<Plug>BujoAddnormal")
-vim.keymap.set("i", "<C-a>", "<Plug>BujoAddinsert")
+vim.keymap.set("n", "<C-a>", "<Plug>BujoAddnormal", { desc = "add an item in normal mode" })
+vim.keymap.set("i", "<C-a>", "<Plug>BujoAddinsert", { desc = "add an item in insert mode" })
 
 -- mark done
-vim.keymap.set("n", "<C-x>", "<Plug>BujoChecknormal")
-vim.keymap.set("i", "<C-x>", "<Plug>BujoCheckinsert")
+vim.keymap.set("n", "<C-x>", "<Plug>BujoChecknormal", { desc = "remove an item in normal mode" })
+vim.keymap.set("i", "<C-x>", "<Plug>BujoCheckinsert", { desc = "remove an item in insert mode" })
 
 vim.g.bujo_window_width = 40
