b6e9c84292bab31e284f6997b2ba39ddaf645472
James Grant <jamespgrant3@gmail.com>
2023-05-24 23:49:16 -0400

    updated setup script to use worktrees

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd $HOME/.config
-ln -s $HOME/repos/nvim-lua nvim
+ln -s $HOME/repos/nvim-lua/master nvim
+
+# if not using git worktree, then use this line:
+# ln -s $HOME/repos/nvim-lua nvim
