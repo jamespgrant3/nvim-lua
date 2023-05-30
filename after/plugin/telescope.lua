099c4826ca07783b6d4fe71905cc60d7ece7e233
James Grant <jamespgrant3@gmail.com>
2023-05-30 11:27:14 -0400

    updated grep_string to search selected word, added fw to search for a word

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fs', function()
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fw', function()
 	builtin.grep_string({ search = vim.fn.input("grep > ") });
 end)
