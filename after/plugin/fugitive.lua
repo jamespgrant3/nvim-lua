dd0fbcd4cc9c2f032a014bb278fdf8146dc61ec0
James Grant <jamespgrant3@gmail.com>
2023-08-18 09:58:30 -0400

    updated git mappings back to being shorter commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,12 +1,12 @@
-vim.keymap.set("n", "<leader>gits", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gitb", function() vim.cmd.Git { args = { 'blame' }} end)
-vim.keymap.set("n", "<leader>gitc", function() vim.cmd.Git { args = { 'commit' }}  end)
-vim.keymap.set("n", "<leader>gitd", function() vim.cmd.Git { args = { 'diff' }} end)
-vim.keymap.set("n", "<leader>gitl", function() vim.cmd.Git { args = { 'log' }} end)
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gb", function() vim.cmd.Git { args = { 'blame' }} end)
+vim.keymap.set("n", "<leader>gc", function() vim.cmd.Git { args = { 'commit' }}  end)
+vim.keymap.set("n", "<leader>gd", function() vim.cmd.Git { args = { 'diff' }} end)
+vim.keymap.set("n", "<leader>gl", function() vim.cmd.Git { args = { 'log' }} end)
 
 -- push to remote without setting upstream
 --vim.keymap.set("n", "<leader>gp", ":Git -c push.default=current push --no-verify<CR>");
 --vim.keymap.set("n", "<leader>gitp", function() vim.cmd.Git { args = { '-c push.default=current push --no-verify' }}  end)
-vim.keymap.set("n", "<leader>gitp", function() vim.cmd.Git { args = { '-c push.default=current push' }}  end)
-vim.keymap.set("n", "<leader>gitf", function() vim.cmd.Git { args = { 'fetch' }} end)
-vim.keymap.set("n", "<leader>gitr", function() vim.cmd.Git { args = { 'rebase' }} end)
+vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git { args = { '-c push.default=current push' }}  end)
+vim.keymap.set("n", "<leader>gf", function() vim.cmd.Git { args = { 'fetch' }} end)
+vim.keymap.set("n", "<leader>gr", function() vim.cmd.Git { args = { 'rebase' }} end)
