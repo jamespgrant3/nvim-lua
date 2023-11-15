08a30ed2f641538d5e8b1d06744e03007b57b26c
James Grant <jamespgrant3@gmail.com>
2023-11-14 22:55:38 -0500

    updated tab colors for selected tab clarity

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,6 +4,15 @@ function ColorMyPencils(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+  vim.api.nvim_set_hl(0, "TabLine", { fg = "#bd93f9", bg = "none" })
+  vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TabLineSel", {
+    fg = "#50fa7b",
+    bold = true,
+    standout = true,
+    bg = "none"
+  })
 end
 
 ColorMyPencils()
--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,11 +1,12 @@
 require("mason-lspconfig").setup {
   ensure_installed = {
-    "eslint",   -- linting engine for javaScript / typescript
-    "html",     -- vscode-html-language-server
-    "jsonls",   -- vscode-json-language-server, a language server for JSON and JSON schema
-    "lua_ls",   -- lua language server.
-    "tflint",   -- a pluggable terraform linter that can act as lsp server
-    "tsserver", -- typescript-language-server depends on typescript
-    "yamlls"    -- yaml-language-server
+    "eslint",       -- linting engine for javaScript / typescript
+    "html",         -- vscode-html-language-server
+    "jsonls",       -- vscode-json-language-server, a language server for JSON and JSON schema
+    "lua_ls",       -- lua language server.
+    "terraformls",  -- terraform language server
+    "tflint",       -- a pluggable terraform linter that can act as lsp server
+    "tsserver",     -- typescript-language-server depends on typescript
+    "yamlls"        -- yaml-language-server
   }
 }
