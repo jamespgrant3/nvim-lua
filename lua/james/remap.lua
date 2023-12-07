e7dda18525d471c3dfd519abbafa13c00c3db59d
James Grant <jamespgrant3@gmail.com>
2023-12-06 22:18:28 -0500

    added remap descriptions

--- lua/james/remap.lua
+++ lua/james/remap.lua
@@ -4,7 +4,8 @@ vim.g.mapleader = " "
 -- nvim tree bindings
 vim.keymap.set("n", "<leader>tt", "<cmd>:NvimTreeToggle<cr>", { desc = "open or close the tree", silent = true })
 vim.keymap.set("n", "<leader>tf", "<cmd>:NvimTreeFocus<cr>", { desc = "open if closed, and focus", silent = true })
-vim.keymap.set("n", "<leader>ti", "<cmd>:NvimTreeFindFile<cr>", { desc = "move cursor to tree for current buffer", silent = true })
+vim.keymap.set("n", "<leader>ti", "<cmd>:NvimTreeFindFile<cr>",
+  { desc = "move cursor to tree for current buffer", silent = true })
 vim.keymap.set("n", "<leader>tc", "<cmd>:NvimTreeCollapse<cr>", { desc = "collapse tree recursively", silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move line down" })
@@ -28,26 +29,28 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 -- vertical insert, have to press esc, map c-c to esc
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "q:", "<nop>")
+vim.keymap.set("n", "q:", "<nop>", { desc = "avoid that weird screen", silent = true })
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "format the current buffer" })
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz",
+  { desc = "display the [count] next error in the list that includes a file name" })
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz",
+  { desc = "display the [count] previous error in the list that includes a file name" })
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "string replace" })
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "make file executable", silent = true })
 
-vim.keymap.set("n", "<leader>q", "<cmd>:q<CR>", { silent = true })
-vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", "<cmd>:q<CR>", { desc = "quit buffer", silent = true })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "write buffer", silent = true })
 
 -- better split window navigation
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "navigate to the window below", silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "navigate to the window above", silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "navigate to the window to the right", silent = true })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "navigate to the window to the left", silent = true })
 
 -- new window
 vim.keymap.set("n", "<leader>hw", "<C-w><C-s>", { desc = "split window horizontal", silent = true })
@@ -57,7 +60,7 @@ vim.keymap.set("n", "<leader>vw", "<C-w><C-v>", { desc = "split window vertical"
 vim.keymap.set("n", "<C-f>", "<C-w><C-o>")
 
 -- better tab navigation
-vim.keymap.set("n", "<leader>tp", "<cmd>:tabprev<CR>", { desc = "move to the previous tab",  silent = true })
+vim.keymap.set("n", "<leader>tp", "<cmd>:tabprev<CR>", { desc = "move to the previous tab", silent = true })
 vim.keymap.set("n", "<leader>tn", "<cmd>:tabnext<CR>", { desc = "move to the next tab", silent = true })
 vim.keymap.set("n", "<leader>ta", "<cmd>:tabnew<CR>", { desc = "new tab after current", silent = true })
 vim.keymap.set("n", "<leader>tb", "<cmd>:-tabnew<CR>", { desc = "new tab before current", silent = true })
@@ -67,7 +70,7 @@ vim.keymap.set("n", "<leader>tr", "<cmd>:tabm +1<cr>", { desc = "move tab right 
 vim.keymap.set("n", "<leader>tl", "<cmd>:tabm -1<cr>", { desc = "move tab left 1 position", silent = true })
 
 -- toggle line numbering
-vim.keymap.set("n", "<leader>ln", "<cmd>:set rnu!<CR>", { desc = "toggle relative line numbering", silent = true  })
+vim.keymap.set("n", "<leader>ln", "<cmd>:set rnu!<CR>", { desc = "toggle relative line numbering", silent = true })
 
 -- prettier
 vim.keymap.set("n", "<leader>a", "<cmd>%!prettier --stdin-filepath %<CR>", { silent = true })
@@ -75,10 +78,12 @@ vim.keymap.set("n", "<leader>ab", "<cmd>%!prettier components pages posts styles
 
 -- git worktree
 -- <Enter> - switches to that worktree
--- <c-d> - deletes that worktree
--- <c-f> - toggles forcing of the next deletion
-vim.keymap.set("n", "<leader>wt", "<cmd>:lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
-vim.keymap.set("n", "<leader>awt", "<cmd>:lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>")
+-- <C-d> - deletes that worktree
+-- <C-f> - toggles forcing of the next deletion
+vim.keymap.set("n", "<leader>wt", "<cmd>:lua require('telescope').extensions.git_worktree.git_worktrees()<CR>",
+  { desc = "switch git worktree", silent = true })
+vim.keymap.set("n", "<leader>wtc", "<cmd>:lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>",
+  { desc = "create a new git worktree", silent = true })
 
 -- update packer
 vim.keymap.set("n", "<leader>pu", "<cmd>:PackerUpdate<CR>", { desc = "run a packer update" })
@@ -90,5 +95,5 @@ vim.keymap.set("n", "<leader>mu", "<cmd>:Mason<CR>", { desc = "run a mason updat
 vim.keymap.set("n", "<leader>bu", "<cmd>:!brew upgrade<CR>", { desc = "run a brew upgrade" })
 
 -- new buffer
-vim.keymap.set("n", "<leader>bv", "<cmd>:vnew<cr>", { desc = "new vertical buffer", silent = true })
-vim.keymap.set("n", "<leader>bh", "<cmd>:new<cr>", { desc = "new vertical buffer", silent = true })
+vim.keymap.set("n", "<leader>bv", "<cmd>:vnew<cr>", { desc = "new buffer vertical ", silent = true })
+vim.keymap.set("n", "<leader>bh", "<cmd>:new<cr>", { desc = "new buffer horizontal", silent = true })
