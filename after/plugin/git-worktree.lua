f2c1a14453e501c00585017ff45bec25fda3ce88
James Grant <jamespgrant3@gmail.com>
2023-12-08 15:32:34 -0500

    updated nvim-tree to no longer float, added worktree hook to fix nvim-tree bug

--- after/plugin/git-worktree.lua
+++ after/plugin/git-worktree.lua
@@ -1,9 +1,23 @@
-require("git-worktree").setup({
+local Worktree = require("git-worktree")
+
+Worktree.setup({
     change_directory_command = "cd",
     update_on_change = true,
     update_on_change_command = "",
     clearjumps_on_change = true,
     autopush = false,
 })
+
+--  an edge case exists with git worktree and nvim-tree
+--  if you change worktrees before interacting with nvim-tree, nvim-worktree successfully changes directories
+--  however, if you first interact with nvim-tree, then change worktrees, nvim-worktree does not change directories
+--  using this hook to ensure we change directories
+Worktree.on_tree_change(function(op, metadata)
+  local api = require("nvim-tree.api")
+
+  if op == Worktree.Operations.Switch then
+    api.tree.change_root(metadata.path)
+  end
+end)
 
 require("telescope").load_extension("git_worktree")
--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -6,13 +6,17 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup{
   disable_netrw = false,
   view = {
-    float = {
-      enable = true,
-      open_win_config = {
-        width = 90,
-        height = 40
-      }
+    relativenumber = true,
+    width = {
+      min = 50,
     }
+    --float = {
+    --  enable = true,
+    --  open_win_config = {
+    --    width = 90,
+    --    height = 40
+    --  }
+    --}
   },
   actions = {
     open_file = {
