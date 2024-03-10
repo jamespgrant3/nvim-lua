e873880aa02a44958e126a9cb6e7aa14e887be36
James Grant <jamespgrant3@gmail.com>
2024-03-10 13:22:15 -0400

    update to toggleterm to allow easier navigation between worktrees

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,17 +1,17 @@
-local Terminal = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({
-  cmd = "lazygit",
-  hidden = true,
-  direction = "tab",
-  --float_opts = {
-  --  width = 150,
-  --  height = 40,
-  --  border = "curved",
-  --},
-})
-
 function _lazygit_toggle()
+  local Terminal = require('toggleterm.terminal').Terminal
+  local lazygit = Terminal:new({
+    cmd = "lazygit -w " .. vim.fn.getcwd(),
+    hidden = true,
+      direction = "tab",
+    --float_opts = {
+    --  width = 150,
+    --  height = 40,
+    --  border = "curved",
+    --},
+  })
+
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
