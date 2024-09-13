239b0916ef18a3d17da01b489f1f6bdfb2b24e0d
James Grant <jamespgrant3@gmail.com>
2024-09-13 01:42:21 -0400

    added more mappings for lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,10 +2,13 @@ return {
   'neovim/nvim-lspconfig',
   config = function()
     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-    --vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<Leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<Leader>e', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<Leader>[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<Leader>]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>e', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>ls', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>fo', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
   end
 }
