4fbc6e36844c08cb0de549ee8611e5aea2d1f08e
James Grant <jamespgrant3@gmail.com>
2024-09-14 12:27:59 -0400

    updated theme to catppuccin-mocha

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
     require("lualine").setup {
       options = {
         icons_enabled = true,
-        theme = "catppuccin",
+        theme = "catppuccin-mocha",
         --component_separators = { left = "", right = ""},
         component_separators = { left = "", right = "" },
         --section_separators = { left = "", right = ""},
