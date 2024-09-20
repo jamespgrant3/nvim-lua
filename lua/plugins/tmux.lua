026d4bf623f27c4c9349eb1d3091eac8aa6403cb
James Grant <jamespgrant3@gmail.com>
2024-09-20 16:07:00 -0400

    updated tmux mapping, to map to option key

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -4,15 +4,15 @@ return {
   keys = {
     -- navigation
     -- keybindings have to match what's configures in the ~/.tmux.conf
-    { "<C-h>", "<cmd>lua require('tmux').move_left()<cr>", desc = "navigate to the window to the left" },
-    { "<C-j>", "<cmd>lua require('tmux').move_bottom()<cr>", desc = "navigate to the window below" },
     { "<C-k>", "<cmd>lua require('tmux').move_top()<cr>", desc = "navigate to the window above" },
     { "<C-l>", "<cmd>lua require('tmux').move_right()<cr>", desc = "navigate to the window to the right" },
+    { "<C-j>", "<cmd>lua require('tmux').move_bottom()<cr>", desc = "navigate to the window below" },
+    { "<C-h>", "<cmd>lua require('tmux').move_left()<cr>", desc = "navigate to the window to the left" },
 
     -- resize
-    { "<C-a>", "<cmd>lua require('tmux').resize_left()<cr>", desc = "resize pane left" },
-    { "<C-z>", "<cmd>lua require('tmux').resize_bottom()<cr>", desc = "resize pane bottom" },
-    { "<C-w>", "<cmd>lua require('tmux').resize_top()<cr>", desc = "resize pane top" },
-    { "<C-s>", "<cmd>lua require('tmux').resize_right()<cr>", desc = "resize pane right" },
+    { "<M-e>", "<cmd>lua require('tmux').resize_top()<cr>", desc = "resize pane top" },
+    { "<M-f>", "<cmd>lua require('tmux').resize_right()<cr>", desc = "resize pane right" },
+    { "<M-c>", "<cmd>lua require('tmux').resize_bottom()<cr>", desc = "resize pane bottom" },
+    { "<M-s>", "<cmd>lua require('tmux').resize_left()<cr>", desc = "resize pane left" },
   }
 }
