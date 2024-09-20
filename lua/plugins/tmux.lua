5f3f4b116d3226ef36bfedb3daab8ba4705570ad
James Grant <jamespgrant3@gmail.com>
2024-09-20 16:32:55 -0400

    updated tmux mapping

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -10,9 +10,9 @@ return {
     { "<C-h>", "<cmd>lua require('tmux').move_left()<cr>", desc = "navigate to the window to the left" },
 
     -- resize
-    { "<M-e>", "<cmd>lua require('tmux').resize_top()<cr>", desc = "resize pane top" },
-    { "<M-f>", "<cmd>lua require('tmux').resize_right()<cr>", desc = "resize pane right" },
-    { "<M-c>", "<cmd>lua require('tmux').resize_bottom()<cr>", desc = "resize pane bottom" },
-    { "<M-s>", "<cmd>lua require('tmux').resize_left()<cr>", desc = "resize pane left" },
+    { "<M-k>", "<cmd>lua require('tmux').resize_top()<cr>", desc = "resize pane top" },
+    { "<M-l>", "<cmd>lua require('tmux').resize_right()<cr>", desc = "resize pane right" },
+    { "<M-j>", "<cmd>lua require('tmux').resize_bottom()<cr>", desc = "resize pane bottom" },
+    { "<M-h>", "<cmd>lua require('tmux').resize_left()<cr>", desc = "resize pane left" },
   }
 }
