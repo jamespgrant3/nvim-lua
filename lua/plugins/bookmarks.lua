2d1410a47a18096b952c5afd84517a3bc93f9f29
James Grant <jamespgrant3@gmail.com>
2024-09-30 18:34:19 -0400

    added telekasten and bookmarks

---
+++ lua/plugins/bookmarks.lua
@@ -0,0 +1,16 @@
+return {
+  'tomasky/bookmarks.nvim',
+  lazy = false,
+  opts = {
+    save_file = vim.fn.expand "$HOME/.bookmarks",
+  },
+  keys = {
+    { "<leader>bt", "<cmd>lua require('bookmarks').bookmark_toggle()<cr>",    desc = "toggle bookmark" },
+    { "<leader>bn", "<cmd>lua require('bookmarks').bookmark_next()<cr>",      desc = "next bookmark" },
+    { "<leader>bp", "<cmd>lua require('bookmarks').bookmark_prev()<cr>",      desc = "previous bookmark" },
+    { "<leader>bl", "<cmd>lua require('bookmarks').bookmark_list()<cr>",      desc = "list bookmarks" },
+    { "<leader>ba", "<cmd>lua require('bookmarks').bookmark_ann()<cr>",       desc = "add bookmark annotation" },
+    { "<leader>bc", "<cmd>lua require('bookmarks').bookmark_clean()<cr>",     desc = "clear bookmarks in current buffer" },
+    { "<leader>bx", "<cmd>lua require('bookmarks').bookmark_clear_all()<cr>", desc = "clear all bookmarks" },
+  },
+}
--- lua/plugins/telekasten.lua
+++ lua/plugins/telekasten.lua
@@ -3,7 +3,7 @@ local templates = vim.fn.expand("~/repos/nvim/master/lua/plugins/telekasten/temp
 
 return {
   "renerocksai/telekasten.nvim",
-  dependencies = {"nvim-telescope/telescope.nvim"},
+  dependencies = { "nvim-telescope/telescope.nvim" },
   lazy = false,
   opts = {
     home = home,
@@ -15,16 +15,16 @@ return {
     template_new_weekly = templates .. "/new_weekly.md",
   },
   keys = {
-    { "<leader>nf", "<cmd>Telekasten find_notes<cr>", desc = "find notes" },
-    { "<leader>ng", "<cmd>Telekasten search_notes<cr>", desc = "search notes" },
+    { "<leader>nf", "<cmd>Telekasten find_notes<cr>",         desc = "find notes" },
+    { "<leader>ng", "<cmd>Telekasten search_notes<cr>",       desc = "search notes" },
     -- { "<leader>nt", "<cmd>Telekasten goto_today<cr>", desc = "goto todays note" },
-    { "<leader>nl", "<cmd>Telekasten follow_link<cr>", desc = "follow note link" },
-    { "<leader>nn", "<cmd>Telekasten new_note<cr>", desc = "new note" },
+    { "<leader>nl", "<cmd>Telekasten follow_link<cr>",        desc = "follow note link" },
+    { "<leader>nn", "<cmd>Telekasten new_note<cr>",           desc = "new note" },
     { "<leader>nt", "<cmd>Telekasten new_templated_note<cr>", desc = "new templated note" },
     -- { "<leader>nc", "<cmd>Telekasten show_calendar<cr>", desc = "" },
-    { "<leader>nb", "<cmd>Telekasten show_backlinks<cr>", desc = "show notes linked to current note" },
-    { "<leader>nI", "<cmd>Telekasten insert_img_link<cr>", desc = "insert image link" },
-    { "<leader>np", "<cmd>Telekasten panel<cr>", desc = "note panel" },
-    { "<leader>nr", "<cmd>Telekasten rename_note<cr>", desc = "rename note and update links pointing to it" }
+    { "<leader>nb", "<cmd>Telekasten show_backlinks<cr>",     desc = "show notes linked to current note" },
+    { "<leader>nI", "<cmd>Telekasten insert_img_link<cr>",    desc = "insert image link" },
+    { "<leader>np", "<cmd>Telekasten panel<cr>",              desc = "note panel" },
+    { "<leader>nr", "<cmd>Telekasten rename_note<cr>",        desc = "rename note and update links pointing to it" }
   },
 }
