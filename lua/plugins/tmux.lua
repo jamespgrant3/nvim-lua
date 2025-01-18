af3c57ce42c51293950c018a98bc21c1492c6fdb
James Grant <jamespgrant3@gmail.com>
2025-01-18 14:17:05 -0500

    added yazi for file management

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,18 +1,18 @@
 return {
-  'aserowy/tmux.nvim',
+  "aserowy/tmux.nvim",
   opts = {},
   keys = {
     -- navigation
     -- keybindings have to match what's configures in the ~/.tmux.conf
-    { "<C-k>", "<cmd>lua require('tmux').move_top()<cr>", desc = "navigate to the window above" },
-    { "<C-l>", "<cmd>lua require('tmux').move_right()<cr>", desc = "navigate to the window to the right" },
-    { "<C-j>", "<cmd>lua require('tmux').move_bottom()<cr>", desc = "navigate to the window below" },
-    { "<C-h>", "<cmd>lua require('tmux').move_left()<cr>", desc = "navigate to the window to the left" },
+    { "<C-k>", "<cmd>lua require('tmux').move_top()<cr>",      desc = "navigate to the window above" },
+    { "<C-l>", "<cmd>lua require('tmux').move_right()<cr>",    desc = "navigate to the window to the right" },
+    { "<C-j>", "<cmd>lua require('tmux').move_bottom()<cr>",   desc = "navigate to the window below" },
+    { "<C-h>", "<cmd>lua require('tmux').move_left()<cr>",     desc = "navigate to the window to the left" },
 
     -- resize
-    { "<M-k>", "<cmd>lua require('tmux').resize_top()<cr>", desc = "resize pane top" },
-    { "<M-l>", "<cmd>lua require('tmux').resize_right()<cr>", desc = "resize pane right" },
+    { "<M-k>", "<cmd>lua require('tmux').resize_top()<cr>",    desc = "resize pane top" },
+    { "<M-l>", "<cmd>lua require('tmux').resize_right()<cr>",  desc = "resize pane right" },
     { "<M-j>", "<cmd>lua require('tmux').resize_bottom()<cr>", desc = "resize pane bottom" },
-    { "<M-h>", "<cmd>lua require('tmux').resize_left()<cr>", desc = "resize pane left" },
+    { "<M-h>", "<cmd>lua require('tmux').resize_left()<cr>",   desc = "resize pane left" },
   }
 }
