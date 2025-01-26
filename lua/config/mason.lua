1432014c5390f78082df77de617a120e26500a73
James Grant <jamespgrant3@gmail.com>
2025-01-26 15:21:08 -0500

    added fixjson keystroke

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -2,3 +2,7 @@
 vim.keymap.set("n", "<leader>fy",
   "<cmd>!yamlfix --config-file $HOME/repos/dotfiles/master/.config/yamlfix/config.toml %<cr>",
   { desc = "format yaml files using yamlfix", silent = true })
+
+vim.keymap.set("n", "<leader>fj",
+  "<cmd>!fixjson --write %<cr>",
+  { desc = "format json files using fixjson", silent = true })
