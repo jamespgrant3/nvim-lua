5ab44760a6259008af37fff3acc864bccb0da520
James Grant <jamespgrant3@gmail.com>
2025-02-06 20:33:33 -0500

    updated yazi mappings

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -3,14 +3,14 @@ return {
   event = 'VeryLazy',
   keys = {
     {
-      "<leader>ti",
+      "<leader>yc",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
     {
       -- Open in the current working directory
-      "<leader>cw",
+      "<leader>yw",
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
     },
@@ -25,6 +25,15 @@ return {
     open_for_directories = true,
     keymaps = {
       show_help = 'g?',
+      open_file_in_vertical_split = "<c-v>",
+      open_file_in_horizontal_split = "<c-h>",
+      open_file_in_tab = "<c-t>",
+      grep_in_directory = "<c-s>",
+      replace_in_directory = "<c-g>",
+      cycle_open_buffers = "<tab>",
+      copy_relative_path_to_selected_files = "<c-y>",
+      send_to_quickfix_list = "<c-q>",
+      change_working_directory = "<c-w>",
     },
   },
 }
