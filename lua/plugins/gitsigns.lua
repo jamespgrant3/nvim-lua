3eff14166342dfcd1626705bcebfbb34b09510fe
James Grant <jamespgrant3@gmail.com>
2025-02-26 01:11:08 -0500

    added more bindings for gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,10 +1,16 @@
 return {
   'lewis6991/gitsigns.nvim',
+  lazy = false,
   opts = {
     current_line_blame           = true,
     current_line_blame_opts      = {
       delay = 500,
     },
     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+  },
+  keys = {
+    { "<leader>gb", "<cmd>:Gitsigns blame<cr>",     desc = "git blame" },
+    { "]g",         "<cmd>:Gitsigns next_hunk<cr>", desc = "next change" },
+    { "[g",         "<cmd>:Gitsigns prev_hunk<cr>", desc = "prev change" },
   }
 }
--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,6 +1,7 @@
 return {
   'mikavilpas/yazi.nvim',
   event = 'VeryLazy',
+  dependencies = { "folke/snacks.nvim", lazy = true },
   keys = {
     {
       "<leader>yc",
