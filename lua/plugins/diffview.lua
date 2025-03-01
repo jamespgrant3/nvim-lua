373252962c64d37d9494902a92a5066fd322f7cd
James Grant <jamespgrant3@gmail.com>
2025-03-01 16:21:52 -0500

    updated diffview bindings

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -4,8 +4,8 @@ return {
   opts = {
   },
   keys = {
-    { "<leader>dvh", "<cmd>:DiffviewFileHistory %<cr>", desc = "diff file history" },
-    { "<leader>dvo", "<cmd>:DiffviewOpen<cr>",        desc = "diff open" },
-    { "<leader>dvc", "<cmd>:DiffviewClose<cr>",       desc = "diff close" },
+    { "<leader>dh", "<cmd>:DiffviewFileHistory %<cr>", desc = "diff file history" },
+    { "<leader>do", "<cmd>:DiffviewOpen<cr>",          desc = "diff open" },
+    { "<leader>dc", "<cmd>:DiffviewClose<cr>",         desc = "diff close" },
   }
 }
