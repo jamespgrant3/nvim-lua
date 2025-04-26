537e268a48254677076b7ac75948629d59103f62
James Grant <jamespgrant3@gmail.com>
2025-04-26 13:23:53 -0400

    adding some changes to hover preview

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,14 +1,71 @@
+-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+--   vim.lsp.handlers.hover, {
+--     border = "rounded",
+--     max_width = 80,
+--     max_height = 20,
+--   }
+-- )
+
 return {
   "neovim/nvim-lspconfig",
-  keys = {
-    { "gd", "<cmd>lua vim.lsp.buf.definition()<cr>" },
-    { "gr", "<cmd>lua vim.lsp.buf.references()<cr>" },
-    { "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<cr>" },
-    { "<leader>e", "<cmd>lua vim.lsp.buf.rename()<cr>" },
-    { "<leader>[", "<cmd>lua vim.diagnostic.goto_prev()<cr>" },
-    { "<leader>]", "<cmd>lua vim.diagnostic.goto_next()<cr>" },
-    { "K", "<cmd>lua vim.lsp.buf.hover()<cr>" },
-    { "<leader>ls", "<cmd>lua vim.lsp.buf.document_symbol()<cr>" },
-    { "<leader>fo", "<cmd>lua vim.lsp.buf.format()<cr>" },
-  }
+  config = function()
+    local servers = { "lua_ls", "ts_ls", "eslint", "terraformls" }
+
+    local on_attach = function(_, bufnr)
+      local opts = { buffer = bufnr }
+
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "<leader>e", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>[", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "<leader>]", vim.diagnostic.goto_next, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "<leader>ls", vim.lsp.buf.document_symbol, opts)
+      vim.keymap.set("n", "<leader>fo", function()
+        vim.lsp.buf.format { async = true }
+      end, opts)
+    end
+
+    -- Example setting up a language server
+    local lspconfig = require("lspconfig")
+
+    for _, server in ipairs(servers) do
+      local opts = {
+        on_attach = on_attach,
+      }
+
+      if server == "lua_ls" then
+        opts.settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }, -- stop warning about "vim" being undefined
+            },
+          },
+        }
+      elseif server == "eslint" then
+        opts.on_attach = function(client, bufnr)
+          client.server_capabilities.documentFormattingProvider = false
+          on_attach(client, bufnr)
+        end
+      end
+
+      lspconfig[server].setup(opts)
+    end
+
+    local border = "rounded"
+
+    -- patch the floating preview function
+    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+    vim.lsp.util.open_floating_preview = function(contents, syntax, opts, ...)
+      opts = opts or {}
+      opts.border = opts.border or border
+      return orig_util_open_floating_preview(contents, syntax, opts, ...)
+    end
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover,
+      { border = border }
+    )
+  end,
 }
