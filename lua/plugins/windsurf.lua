3454e106a05bef897562f68e0a816dc822e97eb3
James Grant <jamespgrant3@gmail.com>
2025-04-28 12:31:27 -0400

    added windsurf

---
+++ lua/plugins/windsurf.lua
@@ -0,0 +1,8 @@
+return {
+	"Exafunction/windsurf.vim",
+	event = "BufEnter",
+	keys = {
+		{ "<leader>cc", "<cmd>CodeiumChat<cr>", desc = "windsurf chat" },
+		{ "<leader>ct", "<cmd>CodeiumToggle<cr>", desc = "windsurf toggle" },
+	},
+}
