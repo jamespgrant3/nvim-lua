3d42dbf5043aecc3d10f85c38eb200f0e660cce6
James Grant <jamespgrant3@gmail.com>
2026-02-11 19:47:10 -0500

    moving to oil.nvim, temporarily commenting out yazi config

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,40 +1,40 @@
 return {
-	"mikavilpas/yazi.nvim",
-	event = "VeryLazy",
-	dependencies = { "folke/snacks.nvim", lazy = true },
-	keys = {
-		{
-			"<leader>yc",
-			mode = { "n", "v" },
-			"<cmd>Yazi<cr>",
-			desc = "Open yazi at the current file",
-		},
-		{
-			-- Open in the current working directory
-			"<leader>yw",
-			"<cmd>Yazi cwd<cr>",
-			desc = "Open the file manager in nvim's working directory",
-		},
-		{
-			"<leader>tt",
-			"<cmd>Yazi toggle<cr>",
-			desc = "Resume the last yazi session",
-		},
-	},
-	opts = {
-		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = true,
-		keymaps = {
-			show_help = "g?",
-			open_file_in_vertical_split = "<c-v>",
-			open_file_in_horizontal_split = "<c-h>",
-			open_file_in_tab = "<c-t>",
-			grep_in_directory = "<c-s>",
-			replace_in_directory = "<c-g>",
-			cycle_open_buffers = "<tab>",
-			copy_relative_path_to_selected_files = "<c-y>",
-			send_to_quickfix_list = "<c-q>",
-			change_working_directory = "<c-w>",
-		},
-	},
+	-- "mikavilpas/yazi.nvim",
+	-- event = "VeryLazy",
+	-- dependencies = { "folke/snacks.nvim", lazy = true },
+	-- keys = {
+	-- 	{
+	-- 		"<leader>yc",
+	-- 		mode = { "n", "v" },
+	-- 		"<cmd>Yazi<cr>",
+	-- 		desc = "Open yazi at the current file",
+	-- 	},
+	-- 	{
+	-- 		-- Open in the current working directory
+	-- 		"<leader>yw",
+	-- 		"<cmd>Yazi cwd<cr>",
+	-- 		desc = "Open the file manager in nvim's working directory",
+	-- 	},
+	-- 	{
+	-- 		"<leader>tt",
+	-- 		"<cmd>Yazi toggle<cr>",
+	-- 		desc = "Resume the last yazi session",
+	-- 	},
+	-- },
+	-- opts = {
+	-- 	-- if you want to open yazi instead of netrw, see below for more info
+	-- 	open_for_directories = true,
+	-- 	keymaps = {
+	-- 		show_help = "g?",
+	-- 		open_file_in_vertical_split = "<c-v>",
+	-- 		open_file_in_horizontal_split = "<c-h>",
+	-- 		open_file_in_tab = "<c-t>",
+	-- 		grep_in_directory = "<c-s>",
+	-- 		replace_in_directory = "<c-g>",
+	-- 		cycle_open_buffers = "<tab>",
+	-- 		copy_relative_path_to_selected_files = "<c-y>",
+	-- 		send_to_quickfix_list = "<c-q>",
+	-- 		change_working_directory = "<c-w>",
+	-- 	},
+	-- },
 }
