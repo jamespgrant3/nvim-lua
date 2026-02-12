4239597a26151afbba4ef39ec4f35b5afcd436ef
James Grant <jamespgrant3@gmail.com>
2026-02-11 20:24:10 -0500

    tweak oil mapping, getting rid of yazi

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -77,7 +77,7 @@ return {
       keymaps = {
         ["g?"] = { "actions.show_help", mode = "n" },
         ["<CR>"] = "actions.select",
-        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-v>"] = { "actions.select", opts = { vertical = true } },
         ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
         ["<C-t>"] = { "actions.select", opts = { tab = true } },
         ["<C-p>"] = "actions.preview",
